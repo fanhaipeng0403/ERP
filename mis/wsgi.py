65ecd49dfca35a83de936668093e69e05e8a752a
樊海鹏 <fanhaipeng0403@126.com>
2018-11-29 00:34:09 +0800

    修复ｂｕｇ

--- mis/wsgi.py
+++ mis/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+from django.contrib import admin
+
+admin.site.site_header = 'ERP'
+admin.site.site_title = 'ERP'
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mis.settings")
 
 from django.core.wsgi import get_wsgi_application
